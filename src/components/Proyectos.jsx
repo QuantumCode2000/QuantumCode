41518d7b7e2189ee211a8e219d4e65802050bc6c
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2021-09-06 11:21:37 -0400

    modified pages

---
+++ src/components/Proyectos.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/Proyectos.css'
+const Proyectos = () => {
+  return (
+    <React.Fragment>
+      <main className="proyectos">
+        <div className="card">
+          <h1>
+            Web
+          </h1>
+          <p>Proyects</p>
+        </div>
+        <div className="card">
+          <h1>
+            Mobile
+          </h1>
+          <p>Proyects</p>
+        </div>
+      </main>
+    </React.Fragment>
+  )
+}
+export default Proyectos;
---
+++ src/components/SobreMi.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const SobreMi = () => {
+  return (
+    <React.Fragment>
+      <h1>
+      SobreMi
+      </h1>
+    </React.Fragment>
+  )
+}
+export default SobreMi;

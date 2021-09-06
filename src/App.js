b382593c17a1562848cc2ae719381d263a33843a
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2021-09-06 11:04:33 -0400

    Creation of pages and using ReactRouterDOm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import Header from './components/Header';
+import Layout from './components/Layout'
+import Contacto from './components/Contacto';
+import SobreMi from './components/SobreMi';
+import Proyectos from './components/Proyectos';
+import Home from './components/Home';
 import './App.css';
+import { BrowserRouter,Route,Switch} from 'react-router-dom';
 function App() {
 
-  
   return (
-    <React.Fragment>
-      <main className="app">
-        <Header/>
-      </main>
-    </React.Fragment>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/sobremi" component={SobreMi}/>
+          <Route exact path="/proyectos" component={Proyectos}/>
+          <Route exact path="/contacto" component={Contacto}/>
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
